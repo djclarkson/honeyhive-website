40f56e6e37a62014193be89ba136fe3c8460fdc6
djclarkson <dave@launchspeed.com>
2025-05-13 14:54:48 +0000

    Deploying to gh-pages from @ djclarkson/honeyhive-website@3dd8ff761cabec950eddd98377f6fe61cb69e4aa 🚀

--- env-config.js
+++ env-config.js
@@ -1,8 +1,8 @@
 // This file is generated at build time
 // It allows environment variables to be available globally in the browser
 window.env = {
-  CLOUDFLARE_TURNSTILE_SITE_KEY: '',
-  VITE_TURNSTILE_SITE_KEY: '',
-  VITE_SUPABASE_URL: '',
-  VITE_SUPABASE_ANON_KEY: ''
+  CLOUDFLARE_TURNSTILE_SITE_KEY: '0x4AAAAAABct_IelMqjb727G',
+  VITE_TURNSTILE_SITE_KEY: '0x4AAAAAABct_IelMqjb727G',
+  VITE_SUPABASE_URL: 'https://cjtzdseuwkgqqbpjecjz.supabase.co',
+  VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdHpkc2V1d2tncXFicGplY2p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNDk1NzAsImV4cCI6MjA2MjYyNTU3MH0.O7aX-K4eF_UeNIDZ0TTl9Ld8Cjuw0exZCKB0ZNvn8mI'
 }; 
--- init-env.js
+++ init-env.js
@@ -2,10 +2,10 @@
 // It initializes environment variables before the main app loads
 
 // Define global Supabase variables
-window.VITE_SUPABASE_URL = "";
-window.VITE_SUPABASE_ANON_KEY = "";
+window.VITE_SUPABASE_URL = "https://cjtzdseuwkgqqbpjecjz.supabase.co";
+window.VITE_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdHpkc2V1d2tncXFicGplY2p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNDk1NzAsImV4cCI6MjA2MjYyNTU3MH0.O7aX-K4eF_UeNIDZ0TTl9Ld8Cjuw0exZCKB0ZNvn8mI";
 window.env = {
-  CLOUDFLARE_TURNSTILE_SITE_KEY: ""
+  CLOUDFLARE_TURNSTILE_SITE_KEY: "0x4AAAAAABct_IelMqjb727G"
 };
 
 console.log("Environment initialization complete");
